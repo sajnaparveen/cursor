c34cb66626b6c0b3b9e31f840aede07c41b1adc7
platosys technologies <platosystechnologies@platosyss-iMac.local>
2022-10-15 18:38:01 +0530

    cursor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
+import {react,useState} from 'react'
 import './App.css';
 
 function App() {
+  const [coord,setCoord]=useState([])
+  const [status,setStatus]=useState()
+
+  const handleMouseMove=(event)=>{
+    let coordnates=[event.clientX,event.clientY]
+    console.log(coord)
+    setCoord(coordnates)
+
+  }
+  let aaa=(value)=>{
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <svg   height={window.outerHeight} width={window.outerWidth} onMouseMove={(event)=>handleMouseMove(event)}
+onMouseOut={aaa()}  >
+<foreignObject x={coord[0]} y={coord[1]} height="100" width="100">
+  <div>
+<h2>X:{coord[0]}</h2>
+<h2>Y:{coord[1]}</h2>
+</div>
+</foreignObject>
+    </svg>
+  
   );
 }
 
